afc466d0edc51c3493fc8ef2666506a58828159e
rasim <rasim44440@gmail.com>
2025-03-14 17:20:30 +0530

    add user signup backed

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { connectToDatabase } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/User";
+
+export async function POST(request:NextRequest){
+    try {
+        const { email , password } = await request.json()
+        if (!email || !password) {
+            return NextResponse.json(
+                {error:"Email and passoword are requried"},
+                {status:400}
+            )
+        }
+        await connectToDatabase()
+        const existingUser = await User.findOne({email})
+
+        if (existingUser) {
+            return NextResponse.json(
+                {error:"User already exists "},
+                {status:400}
+            )
+        }
+
+        await User.create({
+            email,
+            password
+        })
+
+        return NextResponse.json(
+            {message:"User created succefully"},
+            {status:201}
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            {error:"Failed to create user "},
+            {status:500}
+        )
+    }
+}
